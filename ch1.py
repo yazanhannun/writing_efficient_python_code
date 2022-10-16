467e3ebee57aa8485d1372687eb9317b40b61511
Yazan Hannun <yazan_hannun@hotmail.com>
2022-10-17 02:08:22 +0300

    built-in practice:range

--- ch1.py
+++ ch1.py
@@ -132,3 +132,14 @@ list(range(2,11,2))
 
 [2, 4, 6, 8, 10]
 '''
+# Create a range object that goes from 0 to 5
+nums = range(0, 6)
+print(type(nums))
+
+# Convert nums to a list
+nums_list = list(nums)
+print(nums_list)
+
+# Create a new list of odd numbers from 1 to 11 by unpacking a range object
+nums_list2 = [*range(1,12,2)]
+print(nums_list2)
