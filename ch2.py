799f6f7674eb7028c82bd3b84a88b7a9630159c9
Yazan Hannun <yazan_hannun@hotmail.com>
2022-10-19 01:22:12 +0300

    Using %timeit: formal name or literal syntax

--- ch2.py
+++ ch2.py
@@ -22,18 +22,18 @@ ms	        millisecond	    10-3
 s	        second	        100
 '''
 
-# Create a list of integers (0-50) using list comprehension
-nums_list_comp = [num for num in range(51)]
-print(nums_list_comp)
+# # Create a list of integers (0-50) using list comprehension
+# nums_list_comp = [num for num in range(51)]
+# print(nums_list_comp)
 
-# Create a list of integers (0-50) by unpacking range
-nums_unpack = [*range(51)]
-print(nums_unpack)
+# # Create a list of integers (0-50) by unpacking range
+# nums_unpack = [*range(51)]
+# print(nums_unpack)
 
-from timeit import timeit
-time_exe1 = timeit("""nums_list_comp = [num for num in range(51)]""")
-time_exe2 = timeit("""nums_unpack = [*range(51)]""")
-print(time_exe1,time_exe2)
+# from timeit import timeit
+# time_exe1 = timeit("""nums_list_comp = [num for num in range(51)]""")
+# time_exe2 = timeit("""nums_unpack = [*range(51)]""")
+# print(time_exe1,time_exe2)
 
 '''
 Question:
@@ -51,3 +51,63 @@ Unpacking the range object was faster than list comprehension.  (Answer:True)
 
 Both methods had the same runtime.
 '''
+
+#----------------------Using %timeit: specifying number of runs and loops-----------------------#
+'''
+A list of 480 superheroes has been loaded into your session (called heroes). You'd like to analyze 
+the runtime for converting this heroes list into a set. Instead of relying on the default settings 
+for %timeit, you'd like to only use 5 runs and 25 loops per each run.
+
+What is the correct syntax when using %timeit and only using 5 runs with 25 loops per each run?
+
+Possible Answers:
+
+timeit -runs5 -loops25 set(heroes)
+%%timeit -r5 -n25 set(heroes)
+%timeit set(heroes), 5, 25
+%timeit -r5 -n25 set(heroes) True
+'''
+
+#----------------------Using %timeit: formal name or literal syntax-----------------------#
+'''
+Python allows you to create data structures using either a formal name or a literal syntax. 
+In this exercise, you'll explore how using a literal syntax for creating a data structure 
+can speed up runtimes.
+
+data structure	    formal name	    literal syntax
+list	                list()	           []
+dictionary	            dict()	           {}
+tuple	                tuple()	           ()
+'''
+# Create a list using the formal name
+formal_list = list()
+print(formal_list)
+
+# Create a list using the literal syntax
+literal_list = []
+print(literal_list)
+
+# Print out the type of formal_list
+print(type(formal_list))
+
+# Print out the type of literal_list
+print(type(literal_list))
+
+from timeit import timeit
+time_exe1 = timeit("""formal_list = list()""")
+time_exe2 = timeit("""literal_list = []""")
+print(time_exe1, time_exe2)
+
+'''
+Question
+Use %timeit in your IPython console to compare runtimes between creating a list using the formal 
+name (list()) and the literal syntax ([]). Don't include the print() statements when timing.
+Which naming convention is faster?
+
+Possible Answers:
+
+Using the formal name (list()) to create a list is faster.
+
+Using the literal syntax ([]) to create a list is faster. True
+
+Both naming conventions have the same runtime.'''
