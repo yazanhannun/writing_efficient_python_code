2d7d2ae3bd115034c165819e3d98b91b975bcc8b
Yazan Hannun <yazan_hannun@hotmail.com>
2022-10-19 00:43:13 +0300

    Using %timeit

---
+++ ch2.py
@@ -0,0 +1,53 @@
+#----------------------Timing and profiling code-----------------------#
+'''
+In this chapter, you will learn how to gather and compare runtimes between different coding 
+approaches. You'll practice using the line_profiler and memory_profiler packages to profile 
+your code base and spot bottlenecks. Then, you'll put your learnings to practice by replacing 
+these bottlenecks with efficient Python code.'''
+
+#----------------------Examining runtime-----------------------#
+#----------------------Using %timeit: your turn!-----------------------#
+'''
+You'd like to create a list of integers from 0 to 50 using the range() function. However, you 
+are unsure whether using list comprehension or unpacking the range object into a list is faster. 
+Let's use %timeit to find the best implementation.
+
+For your convenience, a reference table of time orders of magnitude is provided below (faster at 
+the top).
+
+symbol	    name	        unit (s)
+ns	        nanosecond	    10-9
+µs (us)	    microsecond	    10-6
+ms	        millisecond	    10-3
+s	        second	        100
+'''
+
+# Create a list of integers (0-50) using list comprehension
+nums_list_comp = [num for num in range(51)]
+print(nums_list_comp)
+
+# Create a list of integers (0-50) by unpacking range
+nums_unpack = [*range(51)]
+print(nums_unpack)
+
+from timeit import timeit
+time_exe1 = timeit("""nums_list_comp = [num for num in range(51)]""")
+time_exe2 = timeit("""nums_unpack = [*range(51)]""")
+print(time_exe1,time_exe2)
+
+'''
+Question:
+Use %timeit within your IPython console (i.e. not within the script.py window) to compare the 
+runtimes for creating a list of integers from 0 to 50 using list comprehension vs. unpacking the 
+range object. Don't include the print() statements when timing.
+
+Which method was faster?
+
+Possible Answers
+
+List comprehension was faster than unpacking range().
+
+Unpacking the range object was faster than list comprehension.  (Answer:True)
+
+Both methods had the same runtime.
+'''
